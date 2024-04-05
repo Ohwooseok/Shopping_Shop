8b679f5f7d526563a6faed47c7ac3dd19e78565a
Ohwooseok <63221878+Ohwooseok@users.noreply.github.com>
2024-04-05 18:01:56 +0900

    1주차 과제 제출(Controller 작성)

--- src/main/java/com/likelion/lionShop/Controller/UserController.java
+++ src/main/java/com/likelion/lionShop/Controller/UserController.java
@@ -1,8 +1,12 @@
 package com.likelion.lionShop.Controller;
 
+import com.likelion.lionShop.Dto.CreateOrderRequestDto;
+import com.likelion.lionShop.Dto.CreateUserRequestDto;
+import com.likelion.lionShop.Dto.UpdateUserRequestDto;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j //로그 출력을 도와주는 어노테이션
 @RestController
@@ -11,10 +15,34 @@ public class UserController {
 
     // 1. 사용자를 생성하는 컨트롤러를 만듭니다.
     // 이때 log.info를 이용하여 사용자의 이름, 주소, ID, PW를 출력해줍니다. return 값은 "사용자 생성"입니다.
+    @PostMapping("/post")
+    public String createUser(@RequestBody CreateUserRequestDto createUserRequestDto){
+        log.info(createUserRequestDto.getName());
+        log.info(createUserRequestDto.getAddress());
+        log.info(createUserRequestDto.getId());
+        log.info(createUserRequestDto.getPassword());
+        return "사용자 생성";
+    }
     // 2. 사용자를 조회하는 컨트롤러를 만듭니다.
     // 이때 log.info를 이용하여 사용자의 ID를 출력해줍니다. return 값은 "사용자 조회"입니다.
+    @GetMapping("{user_id}")
+    public String getUser(@PathVariable long user_id){
+        log.info(String.valueOf(user_id));
+        return "사용자 조회";
+    }
     // 3. 사용자를 수정하는 컨트롤러를 만듭니다.
     // 이때 log.info를 이용를여 사용자의 이름, 주소를 출력해줍니다. return 값은 "사용자 수정"입니다.
+    @PutMapping("/{user_id}/update")
+    public String updateUser(@RequestBody UpdateUserRequestDto updateUserRequestDto, @PathVariable long user_id){
+        log.info(updateUserRequestDto.getName());
+        log.info(updateUserRequestDto.getAddress());
+        return "사용자 수정";
+    }
     // 4. 사용자를 삭제하는 컨트롤러를 만듭니다.
-    // 이때 log.info 이용하여 사용자의 ID를 출력해줍니다. return 값은 "사용자 삭제"입니다.
+    // 이때 log.info 이용하여 사용자의 ID를 출력해줍tm니다. return 값은 "사용자 삭제"입니다.
+    @DeleteMapping("/{user_id}")
+    public String deleteUser(@PathVariable long user_id){
+        log.info(String.valueOf(user_id));
+        return "사용자 삭제";
+    }
 }
