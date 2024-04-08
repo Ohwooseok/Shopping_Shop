ed003845406a22ae3f11934815778afe28f86ee2
Ohwooseok <63221878+Ohwooseok@users.noreply.github.com>
2024-04-08 16:51:43 +0900

    1주차 과제 제출 (리뷰에 맞게 수정)

--- src/main/java/com/likelion/lionShop/Controller/OrderController.java
+++ src/main/java/com/likelion/lionShop/Controller/OrderController.java
@@ -18,18 +18,18 @@ public class OrderController {
         return "주문 생성하기";
     }
     // 2. 주문을 가져오는 컨트롤러를 만듭니다. 이때 return 값은 "주문 가져오기"입니다.
-    @GetMapping("/{order_id}")
-    public String getOrder(@PathVariable long order_id){
+    @GetMapping("/{orderId}")
+    public String getOrder(@PathVariable Long orderId){
         return "주문 가져오기";
     }
     // 3. 주문을 수정하는 컨트롤러를 만듭니다. 이때 return 값은 "주문 수정하기"입니다.
-    @PutMapping("/{order_id}/update")
-    public String updateOrder(@RequestBody UpdateOrderRequestDto updateOrderRequestDto, @PathVariable long order_id ){
+    @PutMapping("/{orderId}/update")
+    public String updateOrder(@RequestBody UpdateOrderRequestDto updateOrderRequestDto, @PathVariable long orderId ){
         return "주문 수정하기";
     }
     // 4. 주문을 삭제하는 컨트롤러를 만듭니다. 이때 return 값은 "주문 삭제하기"입니다.
-    @DeleteMapping("/{order_id}")
-    public String deleteOrder(@PathVariable long order_id){
+    @DeleteMapping("/{orderId}")
+    public String deleteOrder(@RequestParam("id") Long orderId){
         return "주문 삭제하기";
     }
 
--- src/main/java/com/likelion/lionShop/Controller/UserController.java
+++ src/main/java/com/likelion/lionShop/Controller/UserController.java
@@ -1,12 +1,10 @@
 package com.likelion.lionShop.Controller;
 
-import com.likelion.lionShop.Dto.CreateOrderRequestDto;
 import com.likelion.lionShop.Dto.CreateUserRequestDto;
 import com.likelion.lionShop.Dto.UpdateUserRequestDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @Slf4j //로그 출력을 도와주는 어노테이션
 @RestController
@@ -25,24 +23,24 @@ public class UserController {
     }
     // 2. 사용자를 조회하는 컨트롤러를 만듭니다.
     // 이때 log.info를 이용하여 사용자의 ID를 출력해줍니다. return 값은 "사용자 조회"입니다.
-    @GetMapping("{user_id}")
-    public String getUser(@PathVariable long user_id){
-        log.info(String.valueOf(user_id));
+    @GetMapping("{userId}")
+    public String getUser(@PathVariable Long userId){
+        log.info(String.valueOf(userId));
         return "사용자 조회";
     }
     // 3. 사용자를 수정하는 컨트롤러를 만듭니다.
     // 이때 log.info를 이용를여 사용자의 이름, 주소를 출력해줍니다. return 값은 "사용자 수정"입니다.
-    @PutMapping("/{user_id}/update")
-    public String updateUser(@RequestBody UpdateUserRequestDto updateUserRequestDto, @PathVariable long user_id){
+    @PutMapping("/{userId}/update")
+    public String updateUser(@RequestBody UpdateUserRequestDto updateUserRequestDto, @PathVariable long userId){
         log.info(updateUserRequestDto.getName());
         log.info(updateUserRequestDto.getAddress());
         return "사용자 수정";
     }
     // 4. 사용자를 삭제하는 컨트롤러를 만듭니다.
     // 이때 log.info 이용하여 사용자의 ID를 출력해줍tm니다. return 값은 "사용자 삭제"입니다.
-    @DeleteMapping("/{user_id}")
-    public String deleteUser(@PathVariable long user_id){
-        log.info(String.valueOf(user_id));
+    @DeleteMapping("/{userId}")
+    public String deleteUser(@RequestParam("id") Long userId){
+        log.info(String.valueOf(userId));
         return "사용자 삭제";
     }
 }
